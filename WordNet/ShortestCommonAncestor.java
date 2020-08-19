e78091b451a663477eb8c5d9c09bcc64a186fc01
Ryan Albertson <44984886+ryanalbertson@users.noreply.github.com>
2020-08-19 00:48:19 -0400

    worked on wordnet

--- WordNet/ShortestCommonAncestor.java
+++ WordNet/ShortestCommonAncestor.java
@@ -9,6 +9,42 @@ public class ShortestCommonAncestor {
     // constructor takes a rooted DAG as argument
     public ShortestCommonAncestor(Digraph G) {
 
+        if (G == null) throw new IllegalArgumentException("'G' is null.");
+
+        if (!isDAG(G)) throw new IllegalArgumentException("'G' is not a DAG.");
+
+
+    }
+
+
+    /**
+     * Returns true if given digraph is acyclic, false otherwise.
+     *
+     * @param G Digraph object to be tested for acyclicity.
+     */
+    private static boolean isDAG(Digraph G) {
+
+        int numVertices = G.V();
+
+        // Track whether each vertex has been found.
+        boolean[] found = new boolean[numVertices];  // RENAME TO visited  ??
+
+        //
+
+    }
+
+
+    /**
+     * @param G
+     * @param vertex
+     * @param found
+     * @param departures
+     * @param time
+     * @return
+     */
+    private static int DFS(Digraph G, int vertex, boolean[] found,
+                           int[] departures, int time) {
+
 
     }
 
@@ -36,7 +72,7 @@ public class ShortestCommonAncestor {
 
     // a shortest common ancestor of vertex subsets A and B
     public int ancestorSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
-        
+
 
     }
 
--- WordNet/WordNet.java
+++ WordNet/WordNet.java
@@ -1,8 +1,15 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 /**
+ * Builds a {@code WordNet} which implements methods to query the association
+ * between nouns.
  *
+ * @author Ryan Albertson
  */
 public class WordNet {
 
@@ -18,32 +25,83 @@ public class WordNet {
 
 
     /**
-     * Constructs a WordNet by reading in given noun relationships and storing them
+     * Constructs a {@code WordNet} by reading in given noun relationships and storing them
      * in data structures such that the nouns' relationships can be queried.
      *
-     * @param synsets   CSV file containing synsets of nouns.
-     * @param hypernyms CSV file containing hypernyms of the same nouns.
+     * @param synsets   CSV file name containing synsets of nouns.
+     * @param hypernyms CSV file name containing hypernyms of the same nouns.
+     * @throws IllegalArgumentException If 'synsets' or 'hypernoun' is null.
      */
     public WordNet(String synsets, String hypernyms) {
 
         if (synsets == null) throw new IllegalArgumentException("'synsets' is null.");
         if (hypernyms == null) throw new IllegalArgumentException("'hypernyms' is null.");
 
-        
+        this.synsets = new HashMap<Integer, String>();
+        nouns = new HashMap<String, LinkedList<Integer>>();
+
+        // Read synsets and individual nouns into their corresponding hash maps.
+        In synsetsIn = new In(synsets);
+        while (synsetsIn.hasNextLine()) {
+
+            // Read synsets and their indices.
+            StringTokenizer st1 = new StringTokenizer(synsetsIn.readLine(), ",");
+            int synsetIdx = Integer.parseInt(st1.nextToken());
+            String synset = st1.nextToken();
+            this.synsets.put(synsetIdx, synset);
+
+            // Read individual nouns within each synset.
+            StringTokenizer st2 = new StringTokenizer(synset, " ");
+            while (st2.hasMoreTokens()) {
+
+                String noun = st2.nextToken();
+                nouns.getOrDefault(noun, new LinkedList<Integer>()).add(synsetIdx);
+            }
+        }
+        synsetsIn.close();
+
+        // Read synset-hypernym edges into a digraph (not necessarily a DAG).
+        Digraph digraph = new Digraph(this.synsets.size());
+        In hypernymsIn = new In(hypernyms);
+        while (hypernymsIn.hasNextLine()) {
+
+            // Read synset indices
+            StringTokenizer st = new StringTokenizer(hypernymsIn.readLine(), ",");
+            int synsetIdx = Integer.parseInt(st.nextToken());
+
+            // Read corresponding hypernyms.
+            while (st.hasMoreTokens()) {
+
+                // Create edges between synsets and the hypernym(s) they're in.
+                int hypernymIdx = Integer.parseInt(st.nextToken());
+                digraph.addEdge(synsetIdx, hypernymIdx);
+            }
+        }
+        hypernymsIn.close();
+
+        shortestCommonAncestor = new ShortestCommonAncestor(digraph);
     }
 
 
-    // all WordNet nouns
+    /**
+     * Returns an iterable of all nouns in this WordNet.
+     */
     public Iterable<String> nouns() {
 
-
+        return nouns.keySet();
     }
 
 
-    // is the word a WordNet noun?
+    /**
+     * Returns true if given string is a noun in this WordNet, false otherwise.
+     *
+     * @param word String to be checked.
+     */
     public boolean isNoun(String word) {
 
+        if (word == null) throw new IllegalArgumentException("'word' is null.");
 
+        return nouns.containsKey(word);
     }
 
 
