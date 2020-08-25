1bba36581686eb35409d4a7b6361948c1dd14b64
Ryan Albertson <44984886+ryanalbertson@users.noreply.github.com>
2020-08-25 00:18:18 -0400

    worked on seam carver

--- Assignment7/src/main/SeamCarver.java
+++ Assignment7/src/main/SeamCarver.java
@@ -10,105 +10,149 @@ public class SeamCarver {
     // Define pixel energy at the border to be greater than all interior pixels.
     private static final double BORDER_ENERGY = Double.POSITIVE_INFINITY;
 
-    // Store pixel RGB values of the given picture.
-    private int[] rgb;
+    // Define string literals used for RGB calculations.
+    private static final String R = "red";
+    private static final String G = "green";
+    private static final String B = "blue";
 
-    // Store pixel energies of the given picture.
-    private double[] energy;
+    // Store pixel RGB values of current picture.
+    private int[][] rgb;
+
+    // Store pixel energies of the current picture.
+    private double[][] energy;
+
+    // Store current width and height of picture.
+    private int width;
+    private int height;
+
+    // True if current picture is transposed, false otherwise.
+    private boolean isTransposed;
 
 
     /**
      * Constructs a {@code SeamCarver} using the given {@link Picture}.
      *
      * @param picture A {@link Picture}.
+     * @throws IllegalArgumentException If {@code picture} is null.
      */
     public SeamCarver(Picture picture) {
 
         if (picture == null) throw new IllegalArgumentException("'picture' is null.");
 
+        width  = picture.width();
+        height = picture.height();
+        rgb    = new int[width()][height()];
+        energy = new double[width()][height()];
 
+        // Get RGB values of each pixel.
+        for (int col = 0; col < width(); col++) {
+            for (int row = 0; row < height(); row++) {
+                rgb[col][row] = picture.getRGB(col, row);
+            }
+        }
+        // Calculate energy of each pixel using RGB values.
+        for (int col = 0; col < width(); col++) {
+            for (int row = 0; row < height(); row++) {
+                energy[col][row] = calcPixelEnergy(row, col);
+            }
+        }
     }
 
 
     /**
-     * @param col
-     * @param row
-     * @return
-     */
-    private int flatten2DIndex(int col, int row) {
-
-
-    }
-
-
-    /**
-     * @param col
-     * @param row
-     * @return
+     * Calculates the energy of a given pixel using the dual-gradient energy function.
+     *
+     * @param col x-coordinate of given pixel.
+     * @param row y-coordinate of given pixel.
+     * @return Energy value of the given pixel.
      */
     private double calcPixelEnergy(int col, int row) {
 
+        // Assign distinct energy value to border pixels.
+        if (col == 0 || row == 0 || col == width() - 1 || row == height() - 1) {
+            return BORDER_ENERGY;
+        }
 
+        return Math.sqrt(
+            Math.pow(getRGB(col + 1, row, R) - getRGB(col - 1, row, R), 2)
+                + Math.pow(getRGB(col + 1, row, G) - getRGB(col - 1, row, G), 2)
+                + Math.pow(getRGB(col + 1, row, B) - getRGB(col - 1, row, B), 2)
+                + Math.pow(getRGB(col, row + 1, R) - getRGB(col, row - 1, R), 2)
+                + Math.pow(getRGB(col, row + 1, G) - getRGB(col, row - 1, G), 2)
+                + Math.pow(getRGB(col, row + 1, B) - getRGB(col, row - 1, B), 2));
     }
 
 
     /**
-     * @param col
-     * @param row
-     * @return
+     * Uses binary arithmetic to get the red component of an RGB value.
+     *
+     * @param col       x-coordinate of given pixel.
+     * @param row       y-coordinate of given pixel.
+     * @param component The red, green, or blue component of an RGB value.
+     * @return The {@code component} of an RGB value.
+     * @throws IllegalArgumentException If invalid {@code component} is given.
      */
-    private int getRed(int col, int row) {
+    private int getRGB(int col, int row, String component) {
 
+        if (component.equals(R)) return (rgb[col][row] >> 16) & 0xFF;
+        if (component.equals(G)) return (rgb[col][row] >> 8) & 0xFF;
+        if (component.equals(B)) return rgb[col][row] & 0xFF;
 
+        throw new IllegalArgumentException("invalid RGB component");
     }
 
 
     /**
-     * @param col
-     * @param row
-     * @return
+     * Builds and returns a {@link Picture} of the current picture.
      */
-    private int getGreen(int col, int row) {
-
-
-    }
-
-
-    /**
-     * @param col
-     * @param row
-     * @return
-     */
-    private int getBlue(int col, int row) {
-
-
-    }
-
-
-    // current picture
     public Picture picture() {
 
+        //CHECK IF TRANSPOSED
 
+        Picture picture = new Picture(width, height);
+
+        // Fill in picture using current RGB values.
+        for (int col = 0; col < width(); col++) {
+            for (int row = 0; row < height; row++) {
+                picture.setRGB(col, row, rgb[row][col]);
+            }
+        }
+        return picture;
     }
 
 
-    // width of current picture
+    /**
+     * Returns current {@code width} of the picture.
+     */
     public int width() {
 
-
+        return width;
     }
 
 
-    // height of current picture
+    /**
+     * Returns current {@code height} of the picture.
+     */
     public int height() {
 
-
+        return height;
     }
 
 
-    // energy of pixel at column x and row y
-    public double energy(int x, int y) {
+    /**
+     * Returns the energy at the given pixel.
+     *
+     * @param col x-coordinate of given pixel.
+     * @param row y-coordinate of given pixel.
+     * @throws IllegalArgumentException If {@code col} or {@code row}
+     *                                  is out of bounds.
+     */
+    public double energy(int col, int row) {
 
+        //CHECK IF TRANSPOSED
+
+        validate(col, row);
+        return energy[col][row];
 
     }
 
@@ -136,6 +180,30 @@ public class SeamCarver {
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+
+
+    }
+
+
+    /**
+     * Checks that given pixel coordinates are within bounds of the current picture.
+     *
+     * @param col x-coordinate of given pixel.
+     * @param row y-coordinate of given pixel.
+     */
+    private void validate(int col, int row) {
+
+        if (col < 0 || col >= width) throw new IllegalArgumentException("col out of bounds");
+        if (row < 0 || row >= height) throw new IllegalArgumentException("row out of bounds");
+    }
+
+
+    /**
+     * Checks that given seam is non-null, and a valid seam.
+     *
+     * @param seam Sequence of indices representing a seam that's relative to an axis.
+     */
+    private void validate(int[] seam) {
 
 
     }
---
+++ Assignment7/src/tests/main/SeamCarver_Test.java
@@ -0,0 +1,43 @@
+package main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeamCarver_Test {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void picture() {
+    }
+
+    @Test
+    public void width() {
+    }
+
+    @Test
+    public void height() {
+    }
+
+    @Test
+    public void energy() {
+    }
+
+    @Test
+    public void findHorizontalSeam() {
+    }
+
+    @Test
+    public void findVerticalSeam() {
+    }
+
+    @Test
+    public void removeHorizontalSeam() {
+    }
+
+    @Test
+    public void removeVerticalSeam() {
+    }
+}
