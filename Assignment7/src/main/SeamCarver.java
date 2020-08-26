e01afc34b364eb113c4ad6a00cd1e2089a42862b
Ryan Albertson <44984886+ryanalbertson@users.noreply.github.com>
2020-08-25 22:52:54 -0400

    worked on seamcarver

--- Assignment7/src/main/SeamCarver.java
+++ Assignment7/src/main/SeamCarver.java
@@ -56,7 +56,7 @@ public class SeamCarver {
         // Calculate energy of each pixel using RGB values.
         for (int col = 0; col < width(); col++) {
             for (int row = 0; row < height(); row++) {
-                energy[col][row] = calcPixelEnergy(row, col);
+                energy[col][row] = calcPixelEnergy(col, row);
             }
         }
     }
@@ -71,18 +71,20 @@ public class SeamCarver {
      */
     private double calcPixelEnergy(int col, int row) {
 
-        // Assign distinct energy value to border pixels.
-        if (col == 0 || row == 0 || col == width() - 1 || row == height() - 1) {
-            return BORDER_ENERGY;
-        }
+        /* Surrounding pixels used to calculate energy of given pixel. For border
+         * pixels, use opposite column or row instead. */
+        int right = (col == width - 1) ? 0 : col + 1;
+        int left  = (col == 0) ? width - 1 : col - 1;
+        int up    = (row == 0) ? height - 1 : row - 1;
+        int down  = (row == height - 1) ? 0 : row + 1;
 
         return Math.sqrt(
-            Math.pow(getRGB(col + 1, row, R) - getRGB(col - 1, row, R), 2)
-                + Math.pow(getRGB(col + 1, row, G) - getRGB(col - 1, row, G), 2)
-                + Math.pow(getRGB(col + 1, row, B) - getRGB(col - 1, row, B), 2)
-                + Math.pow(getRGB(col, row + 1, R) - getRGB(col, row - 1, R), 2)
-                + Math.pow(getRGB(col, row + 1, G) - getRGB(col, row - 1, G), 2)
-                + Math.pow(getRGB(col, row + 1, B) - getRGB(col, row - 1, B), 2));
+            Math.pow(getRGB(right, row, R) - getRGB(left, row, R), 2)
+                + Math.pow(getRGB(right, row, G) - getRGB(left, row, G), 2)
+                + Math.pow(getRGB(right, row, B) - getRGB(left, row, B), 2)
+                + Math.pow(getRGB(col, up, R) - getRGB(col, down, R), 2)
+                + Math.pow(getRGB(col, up, G) - getRGB(col, down, G), 2)
+                + Math.pow(getRGB(col, up, B) - getRGB(col, down, B), 2));
     }
 
 
@@ -96,6 +98,12 @@ public class SeamCarver {
      * @throws IllegalArgumentException If invalid {@code component} is given.
      */
     private int getRGB(int col, int row, String component) {
+
+        if (isTransposed) {
+            int temp = col;
+            col = row;
+            row = temp;
+        }
 
         if (component.equals(R)) return (rgb[col][row] >> 16) & 0xFF;
         if (component.equals(G)) return (rgb[col][row] >> 8) & 0xFF;
@@ -154,6 +162,7 @@ public class SeamCarver {
      */
     public int width() {
 
+        if (isTransposed) return height;
         return width;
     }
 
@@ -163,6 +172,7 @@ public class SeamCarver {
      */
     public int height() {
 
+        if (isTransposed) return width;
         return height;
     }
 
@@ -178,91 +188,102 @@ public class SeamCarver {
     public double energy(int col, int row) {
 
         validate(col, row);
-        return energy[col][row];
+
+        if (isTransposed) return energy[row][col];
+        else return energy[col][row];
 
     }
 
 
     /**
-     * @return
+     * Returns a least-energy horizontal seam through the picture.
      */
     public int[] findHorizontalSeam() {
 
         if (!isTransposed) transpose();
         keepTranspose = true;
-
-        return findVerticalSeam();
+        int[] seam = findVerticalSeam();
+        keepTranspose = false;
+        return seam;
     }
 
 
     /**
-     * @return
+     * Returns a least-energy vertical seam through the picture. Can also find a
+     * least-energy horizontal seam if picture is transposed beforehand.
      */
     public int[] findVerticalSeam() {
 
-        // Only keep tranposition if horizontal method has called this.
+        // Only keep tranposition if findHorizontalSeam() has called this.
         if (!keepTranspose && isTransposed) transpose();
-
 
         int[] seam = new int[height()];
 
-        int[][]    edgeTo = new int[width()][height()];
+        //
+        int[][] edgeTo = new int[width()][height()];
+
+        // Store cumulative energy of the seams that goes through each pixel.
         double[][] distTo = new double[width()][height()];
 
-        // Initiate distTo to Infinity
+        // Initialize all distances to infinity.
         for (int col = 0; col < width(); col++) {
-            for (int row = 1; row < height(); row++) {
-                distTo[col][row] = Double.POSITIVE_INFINITY;
+            for (int row = 0; row < height(); row++) {
+                if (row == 0) distTo[col][row] = energy[col][row];
+                else distTo[col][row] = Double.POSITIVE_INFINITY;
             }
         }
-        // You should not need recursion. Note that the underlying DAG has such special structure
-        // that you don’t need to compute its topological order explicitly
-        // Run BFS (topological sort) for the implicit DAG to search energy and save at distTo[]
-        // The row-major or column-major traverse order make a difference here
+
+        // Builds least-energy paths through picture.
         for (int row = 1; row < height(); row++) {
             for (int col = 0; col < width(); col++) {
 
-                if (col > 0 && distTo[col - 1][row] > energy[col - 1][row]
-                    + distTo[col][row - 1]) {
+                // 
+                if (col > 0 && distTo[col - 1][row] > (energy[col - 1][row]
+                    + distTo[col][row - 1])) {
 
                     distTo[col - 1][row] = energy[col - 1][row] + distTo[col][row - 1];
                     edgeTo[col - 1][row] = col;
                 }
-                if (distTo[col][row] > energy[col][row]
-                    + distTo[col][row - 1]) {
+                //
+                if (distTo[col][row] > (energy[col][row] + distTo[col][row - 1])) {
 
                     distTo[col][row] = energy[col][row] + distTo[col][row - 1];
                     edgeTo[col][row] = col;
                 }
-                if (col < width() - 1 && distTo[col + 1][row] > energy[col + 1][row]
-                    + distTo[col][row - 1]) {
+                //
+                if (col < width() - 1 && distTo[col + 1][row] > (energy[col + 1][row]
+                    + distTo[col][row - 1])) {
 
                     distTo[col + 1][row] = energy[col + 1][row] + distTo[col][row - 1];
                     edgeTo[col + 1][row] = col;
                 }
             }
         }
-        // Find the seam endpoint of minimum energy at the last line
-        for (int col = 1; col < width() - 1; col++) {
-            if (distTo[seam[height() - 2]][height() - 2] > distTo[col][height() - 2]) {
-                seam[height() - 2] = col;
-            }
+        // Find the end of the least total energy seam.
+        double leastEnergy = Double.POSITIVE_INFINITY;
+        for (int col = 0; col < width() - 1; col++) {
+            double currentEnergy = distTo[col][height() - 1];
+            if (currentEnergy < leastEnergy) seam[height() - 1] = col;
         }
-        // Use edgeTo to backtrack the path of minimum energy
-        for (int row = height() - 2; row > 0; row--) {
+        // Back-track from end of seam to beginning.
+        for (int row = height() - 1; row > 0; row--) {
             seam[row - 1] = edgeTo[seam[row]][row];
         }
         // Follow set at border of test examples
         // helpful to reset energy when shifting array elements
         // cutoff to be 0
         seam[height() - 1] = Math.max(seam[height() - 2] - 1, 0);
-        
+
         return seam;
+
+        // TODO - change distTo -> energyTo
     }
 
 
     /**
-     * @param seam
+     * Removes the given seam from the picture.
+     *
+     * @param seam Array of column indices representing a horizontal seam.
      */
     public void removeHorizontalSeam(int[] seam) {
 
@@ -277,7 +298,9 @@ public class SeamCarver {
 
 
     /**
-     * @param seam
+     * Removes the given seam from the picture.
+     *
+     * @param seam Array of column indices representing a seam.
      */
     public void removeVerticalSeam(int[] seam) {
 
@@ -286,7 +309,22 @@ public class SeamCarver {
         // Only keep transposition if horizontal method has called this.
         if (!keepTranspose && isTransposed) transpose();
 
+        for (int row = 0; row < height(); row++) {
 
+            // Remove the seam pixel. Ignore right border pixel, width-- will remove it.
+            if (!(seam[row] == height() - 1)) {
+                System.arraycopy(rgb[row], seam[row] + 1, rgb[row], seam[row],
+                                 width() - seam[row] - 1);
+            }
+
+        }
+        width--;
+
+        // Update energy for pixels adjacent to the removed seam.
+        for (int row = 1; row < height - 1; row++) {
+            if (seam[row] > 1) energy[seam[row] - 1][row] = calcPixelEnergy(seam[row] - 1, row);
+            if (seam[row] < width - 1) energy[seam[row]][row] = calcPixelEnergy(seam[row], row);
+        }
     }
 
 
@@ -295,6 +333,8 @@ public class SeamCarver {
      *
      * @param col x-coordinate of given pixel.
      * @param row y-coordinate of given pixel.
+     * @throws IllegalArgumentException If either {@code col} or {@code row} is
+     *                                  out of bounds.
      */
     private void validate(int col, int row) {
 
@@ -313,15 +353,18 @@ public class SeamCarver {
      * Checks that given seam is non-null, and a valid seam.
      *
      * @param seam Sequence of indices representing a seam that's relative to an axis.
+     * @throws IllegalArgumentException If {@code seam} is null, out of bounds, or
+     *                                  deviates by more than 1 column or row.
      */
     private void validate(int[] seam) {
 
-        if (isTransposed) {
-            if (seam.length < 1 || seam.length >= width) {
-                throw new IllegalArgumentException("seam is out of bounds");
-            }
-        }
-        else if (seam.length < 1 || seam.length >= height) {
+        // if (isTransposed) {
+        //     if (seam.length < 1 || seam.length >= width) {
+        //         throw new IllegalArgumentException("seam is out of bounds");
+        //     }
+        // }
+        // else
+        if (seam.length < 1 || seam.length >= height) {
             throw new IllegalArgumentException("seam is out of bounds");
         }
         for (int i = 0; i < seam.length; i++) {
